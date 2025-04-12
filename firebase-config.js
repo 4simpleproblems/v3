424fb6d8a32244a9074ced6ea66db55af7c041b9
4simpleproblems <4simpleproblems@gmail.com>
2025-04-11 21:43:35 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCIShXZAgmjZcNTPxxph_SBIFyHyD0KlHM",
+  authDomain: "sp-proj-ee318.firebaseapp.com",
+  projectId: "sp-proj-ee318",
+  storageBucket: "sp-proj-ee318.firebasestorage.app",
+  messagingSenderId: "306497646656",
+  appId: "1:306497646656:web:e87b7bc7b96ffd85506fa5",
+  measurementId: "G-8ZR9HSE4GC"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
